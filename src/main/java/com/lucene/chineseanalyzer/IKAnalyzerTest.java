22f7662531163fe1e0fcee0f699428460ac3f55c
zhaobingcn <zhaobingcn@hotmail.com>
2016-11-24 22:36:39 +0800

    11.24.22

--- src/main/java/com/lucene/chineseanalyzer/IKAnalyzerTest.java
+++ src/main/java/com/lucene/chineseanalyzer/IKAnalyzerTest.java
@@ -13,7 +13,7 @@ import java.io.StringReader;
  */
 public class IKAnalyzerTest {
     //测试数据
-    public static String testData = "基于Neo4j的大规模数据检索技术";
+    public static String testData = "从2006年12月推出1.0版开始，IKAnalyzer已经推出 了3个大版本。最初，它是以开源项目Lucene为应用主体的，结合词典分词和文法分析算法的中文分词组件。新版本的IKAnalyzer3.0则发展为 面向Java的公用分词组件，独立于Lucene项目，同时提供了对Lucene的默认优化实现。";
 
     public void analyzer() throws Exception{
         Analyzer analyzer = new IKAnalyzer();
@@ -24,7 +24,7 @@ public class IKAnalyzerTest {
         OffsetAttribute offsetAttribute = tokenStream.addAttribute(OffsetAttribute.class);
         tokenStream.reset();
         while (tokenStream.incrementToken()){
-            System.out.println(offsetAttribute.toString());
+            System.out.print("[" + offsetAttribute.toString() + "]");
         }
         tokenStream.end();
         tokenStream.close();
